5a0ed2b5ada05c599e0b8c1c2a3b39cbabdbfc70
kobashigawa hiroya <hiroya19890721@gmail.com>
2020-06-29 20:58:49 +0900

    コメント削除機能,編集機能を実装

--- Gemfile
+++ Gemfile
@@ -23,6 +23,8 @@ gem 'jbuilder', '~> 2.7'
 # gem 'bcrypt', '~> 3.1.7'
 # ログイン機能
 gem 'devise'
+gem 'carrierwave'
+gem 'rmagick'
 
 # 日本語化
 gem 'rails-i18n', '~> 6.0'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_21_063313) do
+ActiveRecord::Schema.define(version: 2020_06_21_084545) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 2020_06_21_063313) do
     t.text "content"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "image"
   end
 
 end
