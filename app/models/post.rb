dd1e91946048b73829a746623aee56c21d6b3222
kobashigawa hiroya <hiroya19890721@gmail.com>
2020-07-02 21:25:40 +0900

    bootstrapで投稿一覧表示レイアウト変更

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ApplicationRecord
   mount_uploader :image, ImageUploader
-  validates :title, presence: true, lenght: { maximum: 30 }
-  validates :content, presence: true, lenght: { maximum: 100 }
+  validates :title, presence: true, length: { maximum: 30 }
+  validates :content, presence: true, length: { maximum: 100 }
 end
