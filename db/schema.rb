e9fbe8151e7417c272190653aa66a0a74f3a2b10
kobashigawa hiroya <hiroya19890721@gmail.com>
2020-07-21 21:34:05 +0900

    menu modelを作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_18_051621) do
+ActiveRecord::Schema.define(version: 2020_07_21_122734) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -82,6 +82,14 @@ ActiveRecord::Schema.define(version: 2020_07_18_051621) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_apps_on_email", unique: true
     t.index ["reset_password_token"], name: "index_apps_on_reset_password_token", unique: true
+  end
+
+  create_table "menus", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.string "image"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "posts", force: :cascade do |t|
