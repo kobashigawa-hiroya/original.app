7f577a1a9e1096fe2e930270088c7d05796de0ca
kobashigawa hiroya <hiroya19890721@gmail.com>
2020-07-24 14:59:12 +0900

    Contactモデルmigrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_23_055500) do
+ActiveRecord::Schema.define(version: 2020_07_24_055452) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -82,6 +82,13 @@ ActiveRecord::Schema.define(version: 2020_07_23_055500) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_apps_on_email", unique: true
     t.index ["reset_password_token"], name: "index_apps_on_reset_password_token", unique: true
+  end
+
+  create_table "contacts", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "menus", force: :cascade do |t|
