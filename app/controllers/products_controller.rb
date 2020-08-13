3375dab4e16c4114b5d55e680b747d3ded0da68f
kobashigawa hiroya <hiroya19890721@gmail.com>
2020-08-13 21:15:51 +0900

    ProductsControllerの修正

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,4 @@
-class products_controller < ApplicationController
+class ProductsController < ApplicationController
   def index
     @products = Product.order(created_at: "DESC").includes(:host).page(params[:page]).pae(1)
   end
